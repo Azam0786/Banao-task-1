9210be5279bcde1aa8abe6888c755c8ffe9a56d4
Mohd Azam Siddique <116062064+Azam0786@users.noreply.github.com>
2024-07-03 11:08:30 +0000

    some cchanges

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/todor-andonov-g7RAqJiXUV4-unsplash.jpg";
+import image from "../images/joshua-earle--87JyMb9ZfU-unsplash.jpg";
 
 const imageAltText = "";
 
